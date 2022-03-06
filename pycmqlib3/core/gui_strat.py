74f0af33f8db96249d06e09176077cd837f5ee34
Harvey <harvey_wwu@hotmail.com>
2022-03-06 11:08:37 +0800

    adjust strat_gui
    Former-commit-id: 415507389adb3eb6d8f6bd90ea981912f36668fe

--- pycmqlib3/core/gui_strat.py
+++ pycmqlib3/core/gui_strat.py
@@ -237,8 +237,8 @@ class XSMOMRetGui(StratGui):
 class FactorPortGui(StratGui):
     def __init__(self, strat, app, master):
         StratGui.__init__(self, strat, app, master)
-        self.entry_fields = ['PosScaler', 'IsDisabled']
-        self.status_fields = ['VolWeight', 'CurrPos', 'TargetPos', 'CurrPrices', 'MinTradeSize']
+        self.entry_fields = ['PosScaler', 'IsDisabled', 'AllocW', 'TargetPos']
+        self.status_fields = ['VolWeight', 'CurrPos', 'CurrPrices', 'MinTradeSize']
         self.button_fields = [('LoadFact', 'load_fact_data'), ('TradeTarget', 'trade_target_pos')]
         self.shared_fields = ['IsDisabled', 'PosScaler', 'LoadFact', 'TradeTarget']
         self.field_types = {'RunFlag': 'int',
@@ -246,6 +246,7 @@ class FactorPortGui(StratGui):
                             'CurrPos': 'int',
                             'TargetPos': 'int',
                             'VolWeight': 'float',
+                            'AllocW': 'float',
                             'CurrPrices': 'float',
                             'PosScaler': 'float',
                             'IsDisabled': 'bool',
