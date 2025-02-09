059d75308fc60daee941f2eccfd4f27ef6bc0ac9
Harvey <harvey_wwu@hotmail.com>
2025-02-09 17:26:30 +0800

    update stock data update

--- misc_scripts/auto_update_data_xl.py
+++ misc_scripts/auto_update_data_xl.py
@@ -4,10 +4,11 @@ import win32com.client
 import win32gui
 import win32con
 from pycmqlib3.utility.sec_bits import LOCAL_NUTSTORE_FOLDER, IFIND_XL_HOTKEYS
-from pycmqlib3.utility.dbaccess import write_edb_by_xl_sheet
+from pycmqlib3.utility.dbaccess import write_edb_by_xl_sheet, write_stock_data_by_xl
 
 
-def update_ifind_xlsheet(filename='C:/Users/harvey/Nutstore/1/Nutstore/ifind_data.xlsx', wait_time=40):
+def update_ifind_xlsheet(filename='C:/Users/harvey/Nutstore/1/Nutstore/ifind_data.xlsx', 
+                         wait_time=40, excluded=['hist']):
     xl = win32com.client.DispatchEx("Excel.Application")
     wb = xl.Workbooks.open(filename)
     xl.Visible = True
@@ -15,7 +16,7 @@ def update_ifind_xlsheet(filename='C:/Users/harvey/Nutstore/1/Nutstore/ifind_dat
     time.sleep(10)
     for s in range(len(wb.Sheets)):
         try:
-            if wb.Sheets[s].name in ['hist']:
+            if wb.Sheets[s].name in excluded:
                 continue
             wtime = wait_time
             is_daily = False
@@ -71,9 +72,11 @@ def update_data_from_xl(data_folder=LOCAL_NUTSTORE_FOLDER, lookback=30):
                                         'source': 'ifind', 'reorder': [0, 1, 2, 3], 'drop_zero': False},
     }
     write_edb_by_xl_sheet(file_setup, data_folder=data_folder, lookback=lookback)
+    write_stock_data_by_xl('ifind_stock.xlsx', data_folder=LOCAL_NUTSTORE_FOLDER, lookback=1000)
+
 
 
 if __name__ == "__main__":
-    data_folder = LOCAL_NUTSTORE_FOLDER
-    filename = f'{data_folder}/ifind_data.xlsx'
-    update_ifind_xlsheet(filename=filename)
+    data_folder = LOCAL_NUTSTORE_FOLDER    
+    update_ifind_xlsheet(filename=f'{data_folder}/ifind_data.xlsx', wait_time=40, excluded=['hist'])
+    update_ifind_xlsheet(filename=f'{data_folder}/ifind_stock.xlsx', wait_time=10, excluded=['setup'])
